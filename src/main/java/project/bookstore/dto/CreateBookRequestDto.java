4957cd2a41b8ac24f7a0dd84a3e6d523b6fed50c
Yuliia <innishastudy@gmail.com>
2025-01-29 20:01:16 +0200

    implemented CustomGlobalExceptionHandler and DTO validation

--- src/main/java/project/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/project/bookstore/dto/CreateBookRequestDto.java
@@ -1,14 +1,27 @@
 package project.bookstore.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull(message = "Title is required. Please provide a title.")
     private String title;
+    @NotNull(message = "Author is required. Please provide an author.")
     private String author;
+    @NotNull(message = "ISBN is required. Please provide a valid ISBN number.")
     private String isbn;
+    @NotNull(message = "Price is required. Please provide a price.")
+    @Positive(message = "Price must be greater than 0")
     private BigDecimal price;
+    @NotNull(message = "Description is required. Please provide a book description.")
+    @Size(max = 1000, message = "Description must be less than 1000 characters")
     private String description;
+    @NotNull(message = "Cover image URL is required. Please provide a valid URL.")
+    @URL(message = "Invalid cover image URL. Please provide a valid URL.")
     private String coverImage;
 }
