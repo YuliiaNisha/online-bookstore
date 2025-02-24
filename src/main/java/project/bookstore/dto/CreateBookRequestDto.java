474e3b8fb03a7f2adb106364f73e8a34ef312def
Yuliia Tatarchuk <153382678+YuliiaNisha@users.noreply.github.com>
2025-02-24 14:29:10 +0200

    implemented CustomGlobalExceptionHandler and DTO validation (#5)
    * implemented CustomGlobalExceptionHandler and DTO validation
    
    * replaced validator and added @NotBlank
    
    * improved the code
    
    * removed unnecessary variables
    
    * removed status from exception body

--- src/main/java/project/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/project/bookstore/dto/CreateBookRequestDto.java
@@ -1,14 +1,26 @@
 package project.bookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "Title is required. Please provide a title.")
     private String title;
+    @NotBlank(message = "Author is required. Please provide an author.")
     private String author;
+    @NotBlank(message = "ISBN is required. Please provide a valid ISBN number.")
     private String isbn;
+    @NotNull(message = "Price is required. Please provide a price.")
+    @Positive(message = "Price must be greater than 0")
     private BigDecimal price;
+    @Size(max = 1000, message = "Description must be less than 1000 characters")
     private String description;
+    @URL(message = "Invalid cover image URL. Please provide a valid URL.")
     private String coverImage;
 }
