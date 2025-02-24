b4e2be7e2825a5d7a8adf1905f4865f861e455a6
Yuliia <innishastudy@gmail.com>
2025-02-24 19:42:08 +0200

    implemented Pagination, Sorting, Swagger

--- src/main/java/project/bookstore/service/BookServiceImpl.java
+++ src/main/java/project/bookstore/service/BookServiceImpl.java
@@ -2,6 +2,7 @@ package project.bookstore.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import project.bookstore.dto.BookDto;
@@ -35,8 +36,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable)
                 .stream()
                 .map(bookMapper::toDto)
                 .toList();
@@ -60,9 +61,9 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> search(BookSearchParameters searchParameters) {
+    public List<BookDto> search(BookSearchParameters searchParameters, Pageable pageable) {
         Specification<Book> bookSpecification = bookSpecificationBuilder.build(searchParameters);
-        return bookRepository.findAll(bookSpecification)
+        return bookRepository.findAll(bookSpecification, pageable)
                 .stream()
                 .map(bookMapper::toDto)
                 .toList();
