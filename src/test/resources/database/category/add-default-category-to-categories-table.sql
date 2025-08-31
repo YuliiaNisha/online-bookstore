d3bdded461517e21415a91131c3c9d2362b2ed68
Yuliia <innishastudy@gmail.com>
2025-08-31 15:04:37 +0300

    created tests for book and category

--- src/test/resources/database/category/add-default-category-to-categories-table.sql
+++ src/test/resources/database/category/add-default-category-to-categories-table.sql
@@ -1,3 +1,5 @@
+DELETE FROM categories;
+ALTER TABLE categories AUTO_INCREMENT = 1;
 INSERT INTO categories (id, name, description, is_deleted)
 VALUES
 (1, 'Default category', 'Default category description', FALSE)
