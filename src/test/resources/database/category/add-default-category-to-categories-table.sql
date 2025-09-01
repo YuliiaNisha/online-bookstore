0092ec0d1dfc7fd88e0db3caed1dc6dd9c87c196
Yuliia <innishastudy@gmail.com>
2025-09-01 14:24:41 +0300

    created tests for book and category

--- src/test/resources/database/category/add-default-category-to-categories-table.sql
+++ src/test/resources/database/category/add-default-category-to-categories-table.sql
@@ -1,3 +1,3 @@
 INSERT INTO categories (name, description, is_deleted)
 VALUES
-    ('Default category', 'Default category description', 0);
+('Default category', 'Default category description', 0);
